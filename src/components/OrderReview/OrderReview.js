50d69ded6f17258c7e7572cc79232878f959ea56
sohaghosen <sohag.hosen@outlook.com>
2021-10-14 19:07:07 +0600

    update cart component

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,11 +1,10 @@
 import React from "react";
 import useProducts from "../../hooks/useProducts";
 import useCart from "../../hooks/useCart";
-import Cart from "../Cart/Cart";
 import ReviewItem from "../ReviewItem/ReviewItem";
 import { clearTheCart, removeFromDb } from "../../utilities/fakedb";
 import { useHistory } from "react-router";
-
+import emptyCart from "../../images/emptyCart.jpg";
 const OrderReview = () => {
   const [products] = useProducts();
   const [cart, setCart] = useCart(products);
@@ -24,16 +23,25 @@ const OrderReview = () => {
   };
 
   return (
-    <div className="pt-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 ">
-        {cart.map((product) => (
-          <ReviewItem
-            key={product.key}
-            product={product}
-            handleRemove={handleRemove}
-          />
-        ))}
-      </div>
+    <div className="pt-20 ">
+      {cart.length ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 ">
+          {cart.map((product) => (
+            <ReviewItem
+              key={product.key}
+              product={product}
+              handleRemove={handleRemove}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="flex justify-center items-center">
+          <div>
+            <img className="h-96" src={emptyCart} alt="" />
+            <h3 className="text-3xl mb-7 text-center">Your Cart is Empty</h3>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
